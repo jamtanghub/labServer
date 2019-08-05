8eb3c40e4f88f9f9f3602815f25bd78e0a2418ba
tangjin <lucktangjin@163.com>
2019-08-05 14:31:12 +0800

    '结构初始化'

---
+++ src/main/java/com/jam/labServer/LabServerApplication.java
@@ -0,0 +1,13 @@
+package com.jam.labServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabServerApplication.class, args);
+	}
+
+}
